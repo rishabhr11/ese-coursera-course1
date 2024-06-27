5c722be310e4c94d3f75cc594cdc2095918238a8
RISHABH <154804030+rishabhr11@users.noreply.github.com>
2024-06-27 06:30:02 +0530

    Update stats.c

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -34,9 +34,66 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+void print_statistics(unsigned char *data, unsigned int size) {
+    unsigned char min = find_minimum(data, size);
+    unsigned char max = find_maximum(data, size);
+    unsigned char mean = find_mean(data, size);
+    unsigned char median = find_median(data, size);
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+    printf("Statistics:\n");
+    printf("Minimum: %d\n", min);
+    printf("Maximum: %d\n", max);
+    printf("Mean: %d\n", mean);
+    printf("Median: %d\n", median);
+}
+
+void print_array(unsigned char *data, unsigned int size) {
+    printf("Array:\n");
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+
+unsigned char find_median(unsigned char *data, unsigned int size) {
+    // Implementation of median calculation goes here
+    // You need to implement the median calculation logic
+    // This is a placeholder
+    return 0;
+}
+
+unsigned char find_mean(unsigned char *data, unsigned int size) {
+    unsigned int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += data[i];
+    }
+    return sum / size;
+}
+
+unsigned char find_maximum(unsigned char *data, unsigned int size) {
+    unsigned char max = data[0];
+    for (int i = 1; i < size; ++i) {
+        if (data[i] > max) {
+            max = data[i];
+        }
+    }
+    return max;
+}
+
+unsigned char find_minimum(unsigned char *data, unsigned int size) {
+    unsigned char min = data[0];
+    for (int i = 1; i < size; ++i) {
+        if (data[i] < min) {
+            min = data[i];
+        }
+    }
+    return min;
+}
+
+void sort_array(unsigned char *data, unsigned int size) {
+    // Implementation of sorting algorithm goes here
+    // You need to implement the sorting algorithm (e.g., bubble sort, quicksort)
+    // This is a placeholder
 
 }
 
